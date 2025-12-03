214a70b7a074898b5a873d3720dea75fb06bc50b
kevinmotti <kevinmotti.icarus@gmail.com>
2025-12-03 21:14:38 +0100

    Add files via upload

---
+++ UNIT_1/S2/L3/nome_band.py
@@ -0,0 +1,5 @@
+c = input("qual'è la tua città natale?: ") 
+n = input("qual'è il nome del tuo animale domestico?: ")
+print(f"il nome della tua band potrebbe essere:  {c} {n}")
+
+print("termine del programma") 
---
+++ UNIT_1/S2/L3/pari_dispari.py
@@ -0,0 +1,7 @@
+a = int(input("inserisci un numero: "))
+
+if a % 2 == 0:
+    print("il numero è pari")
+else: print("il numero è dispari")
+
+print("fine programma")
